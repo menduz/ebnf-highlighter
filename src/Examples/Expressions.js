736e4846ea4a7acb070c450ca3154b2af39d38cd
menduz <agustin.mz.92@gmail.com>
2016-10-20 01:18:08 -0300

    Updated expressions example

--- src/Examples/Expressions.js
+++ src/Examples/Expressions.js
@@ -1,36 +1,36 @@
 export const name = "Expressions";
 export const grammar = `
+
 { ws=explicit }
 Document 
-    ::= (WS* Expression WS* ';'?)* EOF {pin=1, ws=implicit}
+    ::= (WS* Expression ([#x20#x09]* EOL | WS* Comment))* EOF {pin=1, ws=implicit}
 
 Expression 
-    ::= (UnaryExpression | BinaryExpression | ParenthesisExpression | SingleExpression) {pin=1, ws=implicit}
+    ::= (UnaryExpression | BinaryExpression | ParenthesisExpression | SingleExpression) {pin=1, ws=implicit, fragment=true}
 UnaryExpression         
-    ::= WS* UnaryOperator WS+ Expression {pin=2}
+    ::= WS* UnaryOperator (WS+|&'(') Expression {pin=2}
 BinaryExpression        
-    ::= WS* ExpressionWithoutBinary WS+ BinaryOperator WS+ Expression{fragment=true,pin=1}
+    ::= WS* ExpressionWithoutBinary WS+ BinaryOperator WS+ Expression{pin=1}
 ParenthesisExpression   
     ::= '(' Expression ')' {ws=implicit,pin=1,fragment=true}
 
 ExpressionWithoutBinary 
     ::= (UnaryExpression | ParenthesisExpression | SingleExpression) {pin=1, ws=implicit,fragment=true}
 
-BinaryOperator ::= 'map' | 'groupBy' | 'and' | 'or' | '~=' | '!=' | '==' | '>=' | '<=' | '>' | '<' | '^=' | '+' | '-' | '*' | '/'
+BinaryOperator ::= 'map' | 'filter'| 'groupBy' | 'joinBy' | 'and' | 'or' | '~=' | '!=' | '==' | '>=' | '<=' | '>' | '<' | '^=' | '+' | '-' | '*' | '/'
 UnaryOperator  ::= "sizeOf" | "upper" | "lower" | "typeOf"
 
-Literal ::= String | Number {ws=implicit}
+Literal ::= String | Number {ws=implicit,fragment=true}
 
 SingleExpression ::= ParenthesisExpression | Literal | Identifier {ws=implicit, fragment=true}
 
 Identifier  ::= ('$$' | '$' | [A-Za-z_])([A-Za-z0-9_]|'.')*
 
 Number      ::= "-"? ("0" | [1-9] [0-9]*) ("." [0-9]+)? (("e" | "E") ( "-" | "+" )? ("0" | [1-9] [0-9]*))? {pin=2}
-String      ::= '"' CHAR* '"'
+String      ::= '"' (!'"' [#x20-#xFFFF])* '"'
 
 Comment     ::= '#' (![#x0A#x0D] [#x00-#xFFFF])* EOL
 
-CHAR        ::= (!'"' [#x20-#xFFFF] | '"')
 WS          ::= Comment | [#x20#x09#x0A#x0D]+ {fragment=true}
 EOL         ::= [#x0A#x0D]+|EOF
 /* EOF */
@@ -40,6 +40,28 @@ EOL         ::= [#x0A#x0D]+|EOF
 export const css = ``;
 
 export const example = `
+   payload.a + "asd"
+  
+  "asd"
+  
+  payload
+  
+  upper "Agustin"
+
+  
+  sizeOf payload filter $.age > 30
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ unary
+#        ^^^^^^^^^^^^^^^^^^^^^^^^^ binary
+#                       ^^^^^^^^^^ binary
+# IS THE SAME AS
+  sizeOf (payload filter $.age > 30)
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ unary
+#        ^^^^^^^^^^^^^^^^^^^^^^^^^^^ binary
+#                        ^^^^^^^^^^ binary
+
+  payload map (upper($.name joinBy ","))
+  # is the same as
+  payload map  upper $.name joinBy ","  
 
   sizeOf a
 # ^^^^^^^^ unary
@@ -64,4 +86,7 @@ export const example = `
 #         ^^^^^^^^^^^^^^^^^^^^^^^            binary
 #                     ^^^^^^^^^^             binary
 #                                   ^^^^^^^^ unary
+
+
+
 `;
